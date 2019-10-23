06680b2a4bf014c20c4ef991e27e779994d90e05
Levinna <kevinna95@gmail.com>
2019-10-23 09:35:36 +0900

    Element UI installation

--- itla/src/main.js
+++ itla/src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'; // import element-ui
+import 'element-ui/lib/theme-chalk/index.css'; // import element-ui (CSS)
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI); // Element UI 전역 등록
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- itla/src/router/index.js
+++ itla/src/router/index.js
@@ -4,29 +4,33 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
-Vue.use(VueRouter) // Router 전역 등록
+Vue.use(VueRouter); // Router 전역 등록
 
 const routes = [ // path connection
-  {  // 이동하는 방법은 path로 이동, name으로 이동하는 방법 2가지가 있음.
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
+    {  // 이동하는 방법은 path로 이동, name으로 이동하는 방법 2가지가 있음.
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'about',
 
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    { // with no path = redirect to root
+        path: '*',
+        redirect: "/"
+    }
+];
 
 const router = new VueRouter({
-  mode: 'history', // link 이동을 쉽게 하기 위함, 아니면 /About 등으로 이동하지 않고 링크가 이상하게 길어짐
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: 'history', // link 이동을 쉽게 하기 위함, 아니면 /About 등으로 이동하지 않고 링크가 이상하게 길어짐
+    base: process.env.BASE_URL,
+    routes
+});
 
 export default router
