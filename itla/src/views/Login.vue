b1e1d2bf8b172ddb14e962cec5424483f244ca90
Levinna <kevinna95@gmail.com>
2019-10-24 11:23:16 +0900

    Login and Signup function updated.

--- itla/src/views/Login.vue
+++ itla/src/views/Login.vue
@@ -1,22 +1,23 @@
 <template>
-    <span class="login">
-        <h1>This is login page</h1> <!-- 추후 다른 스타일로 변경 -->
+    <div class="login">
+        <el-card class="login-card">
+            <h2>Login</h2>
+            <el-form class="loginForm" :model="loginModel" :rules="loginRules" ref="form" @submit.native.prevent="login">
+                <el-form-item prop="email">
+                    <el-input v-model="loginModel.email" placeholder="E-mail" prefix-icon="el-icon-user"></el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input v-model="loginModel.password" placeholder="Password" type="password" prefix-icon="el-icon-lock"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button :loading="loading" class="login-button" type="primary" native-type="submit" block>Login</el-button> <br/>
+                    <el-button class="signup-button" type="success" @click="trySignup">Sign Up</el-button>
+                </el-form-item>
+                <a class="forgot-password" style="text-decoration: none" href="https://localhost:8080/password-recovery">Forgot password ?</a>
+            </el-form>
+        </el-card>
+    </div>
 
-        <el-form :model="loginForm" :rules="rules" ref="loginForm" label-width="120px" class="loginForm" @keyup.enter.native="submitForm('loginForm')">
-            <el-form-item label="E-mail" prop="email">
-                <el-input v-model="loginForm.email" ></el-input>
-            </el-form-item>
-            <el-form-item label="Password" prop="password">
-                <el-input v-model="loginForm.password" show-password></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="success" @click="trySignup">Sign Up</el-button>
-                <el-button type="primary" @click="submitForm('loginForm')">Login</el-button>
-                <!-- <el-link type="primary">Forgot password?</el-link> -->
-            </el-form-item>
-        </el-form>
-
-    </span>
 </template>
 
 <script>
@@ -24,11 +25,16 @@
         name: "Login",
         data() {
             return {
-                loginForm : {
+                loading : false,
+                validCredentials: { // get Valid credential from json file later
+                    email: "test@test.com",
+                    password: "123456"
+                },
+                loginModel : {
                     email : "",
                     password : ""
                 },
-                rules : {
+                loginRules : {
                     email : [
                         {required : true, message : 'Please input the e-mail', trigger :'blur'},
                         {type: 'email', message: 'Please input correct email address', trigger: ['blur', 'change']}
@@ -40,30 +46,85 @@
                 }
             }
         },
+
         methods: {
+
+            simulateLogin() {
+                return new Promise(resolve => {
+                    setTimeout(resolve, 500);
+                });
+            },
+            async login() {
+                let valid = await this.$refs.form.validate();
+                if (!valid) {
+                    return;
+                }
+                this.loading = true;
+                await this.simulateLogin();
+                this.loading = false;
+                if (this.loginModel.email === this.validCredentials.email &&
+                    this.loginModel.password === this.validCredentials.password) {
+                    this.$message.success("Login successfull");
+                    await this.$router.push("/home"); // to Home
+                } else {
+                    this.$message.error("Some information you provided is invalid. Please Try Again.");
+                }
+            },
             trySignup() {
                 //Transition to Signup
                 this.$router.push("/signup");
             },
-            submitForm(formName) {
-                this.$refs[formName].validate((valid) => {
-                    if (valid) {
-                        this.$message({
-                            message: "Login Success!", // + JSON.stringify(this.user), FOR DEBUG
-                            type: 'success'
-                        });
-                        this.$router.push("/home"); // to Home
-                    } else {
-                        //console.log('error submit!!');  // FOR DEBUG
-                        this.$message.error("Some information you provided is invalid. Please Try Again.");
-                        return false;
-                    }
-                });
-            }
+            // submitForm(formName) {
+            //     this.$refs[formName].validate((valid) => {
+            //         if (valid) {
+            //             this.$message({
+            //                 message: "Login Success!", // + JSON.stringify(this.user), FOR DEBUG
+            //                 type: 'success'
+            //             });
+            //             this.$router.push("/home"); // to Home
+            //         } else {
+            //             //console.log('error submit!!');  // FOR DEBUG
+            //             this.$message.error("Some information you provided is invalid. Please Try Again.");
+            //             return false;
+            //         }
+            //     });
+            // }
         }
     }
 </script>
 
 <style scoped>
+    .login {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin:3%;
+    }
+    .login-button, .signup-button{
+        width: 100%;
+        margin-top: 1%;
+    }
+    .login-card {
+        display: flex;
+        justify-content: center;
+        width: 340px;
+        padding-top : 1%;
+        padding-bottom : 1%;
+    }
+    .loginForm {
+        width: 300px;
+    }
+    .forgot-password {
+        margin-top: 10px;
+    }
 
+    h2 {
+        color: black;
+        letter-spacing: 1px;
+        padding-bottom: 20px;
+    }
+    .login .el-input input {
+        padding-left: 35px;
+    }
 </style>
